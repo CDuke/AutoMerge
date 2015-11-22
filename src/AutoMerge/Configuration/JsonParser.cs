902a21133de65f15cb51ba93d0151a75aa2a96d6
Kulikov Denis <cdbiznes@gmail.com>
2015-11-23 00:47:25 +0300

    Fix JsonParser -  add support new line in values. Fix #18

--- src/AutoMerge/Configuration/JsonParser.cs
+++ src/AutoMerge/Configuration/JsonParser.cs
@@ -1,55 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Script.Serialization;
 
 namespace AutoMerge
 {
-    internal static class JsonParser
+    public static class JsonParser
     {
-        public static Dictionary<string, string> ParseJson(string res)
+        public static Dictionary<string, string> ParseJson(string jsonText)
         {
-            var lines = res.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            var ht = new Dictionary<string, string>(20);
-            var st = new Stack<string>(20);
-
-            for (var i = 0; i < lines.Length; ++i)
-            {
-                var line = lines[i];
-                var pair = line.Split(":".ToCharArray(), 2, StringSplitOptions.RemoveEmptyEntries);
-
-                if (pair.Length == 2)
-                {
-                    var key = ClearString(pair[0]);
-                    var val = ClearString(pair[1]);
-
-                    if (val == "{")
-                    {
-                        st.Push(key);
-                    }
-                    else
-                    {
-                        if (st.Count > 0)
-                        {
-                            key = string.Join("_", st) + "_" + key;
-                        }
-
-                        if (ht.ContainsKey(key))
-                        {
-                            ht[key] += "&" + val;
-                        }
-                        else
-                        {
-                            ht.Add(key, val);
-                        }
-                    }
-                }
-                else if (line.IndexOf('}') != -1 && st.Count > 0)
-                {
-                    st.Pop();
-                }
-            }
-
-            return ht;
+            var jss = new JavaScriptSerializer();
+            return jss.Deserialize<Dictionary<string, string>>(jsonText);
         }
 
         public static string ToJson(Dictionary<string, string> dict)
@@ -57,26 +18,6 @@ namespace AutoMerge
             var entries = dict.Select(d =>
                 string.Format("\"{0}\": \"{1}\"", d.Key, d.Value));
             return "{" + Environment.NewLine + "  " + string.Join(",\r\n  ", entries) + Environment.NewLine + "}";
-        }
-
-        private static string ClearString(string str)
-        {
-            str = str.Trim();
-
-            var ind0 = str.IndexOf("\"");
-            var ind1 = str.LastIndexOf("\"");
-
-            if (ind0 != -1 && ind1 != -1)
-            {
-                str = str.Substring(ind0 + 1, ind1 - ind0 - 1);
-            }
-            else if (str[str.Length - 1] == ',')
-            {
-                str = str.Substring(0, str.Length - 1);
-            }
-
-            return str;
-            //str = HttpUtility.UrlDecode(str);
         }
     }
 }
