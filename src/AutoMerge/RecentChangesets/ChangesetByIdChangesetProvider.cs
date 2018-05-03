64b855c2fef5c8e59306f3455b7df8c05bad601a
Ruben Willems <ruben.willems@gmail.com>
2018-05-03 10:31:30 +0200

    nameof operator iso strings for raise event

--- src/AutoMerge/RecentChangesets/ChangesetByIdChangesetProvider.cs
+++ src/AutoMerge/RecentChangesets/ChangesetByIdChangesetProvider.cs
@@ -4,33 +4,33 @@ using System.Linq;
 
 namespace AutoMerge
 {
-	public class ChangesetByIdChangesetProvider : ChangesetProviderBase
-	{
-		private readonly IEnumerable<int> _changesetIds;
+    public class ChangesetByIdChangesetProvider : ChangesetProviderBase
+    {
+        private readonly IEnumerable<int> _changesetIds;
 
-		public ChangesetByIdChangesetProvider(IServiceProvider serviceProvider, IEnumerable<int> changesetIds)
-			: base(serviceProvider)
-		{
-			if (changesetIds == null)
-				throw new ArgumentNullException("changesetIds");
+        public ChangesetByIdChangesetProvider(IServiceProvider serviceProvider, IEnumerable<int> changesetIds)
+            : base(serviceProvider)
+        {
+            if (changesetIds == null)
+                throw new ArgumentNullException(nameof(changesetIds));
 
-			_changesetIds = changesetIds;
-		}
+            _changesetIds = changesetIds;
+        }
 
-		protected override List<ChangesetViewModel> GetChangesetsInternal()
-		{
-			var changesets = new List<ChangesetViewModel>();
-			var changesetService = GetChangesetService();
-			if (changesetService != null)
-			{
-				changesets = _changesetIds
-					.Select(changesetService.GetChangeset)
-					.Where(c => c != null)
-					.Select(tfsChangeset => ToChangesetViewModel(tfsChangeset, changesetService))
-					.ToList();
-			}
+        protected override List<ChangesetViewModel> GetChangesetsInternal()
+        {
+            var changesets = new List<ChangesetViewModel>();
+            var changesetService = GetChangesetService();
+            if (changesetService != null)
+            {
+                changesets = _changesetIds
+                    .Select(changesetService.GetChangeset)
+                    .Where(c => c != null)
+                    .Select(tfsChangeset => ToChangesetViewModel(tfsChangeset, changesetService))
+                    .ToList();
+            }
 
-			return changesets;
-		}
-	}
+            return changesets;
+        }
+    }
 }
